d354313a1dd06a75e924fa9005615158d3395f45
Francisco Rojo <frojo@andrew.cmu.edu>
2016-02-24 01:03:00 -0500

    Setting up character player mallow

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 50f;
+	public float jumpForce = 150f;
+
+	public bool grounded;
+
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+
+		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+	
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void FixedUpdate () {
+
+		float h = Input.GetAxis ("Horizontal");
+
+		rb2d.AddForce ((Vector2.right * speed) * h);
+
+
+	
+	}
+}
