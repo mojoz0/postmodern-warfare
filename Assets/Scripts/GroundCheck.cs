6d3097a4686cd4f9d26d4ce91a8b83fbe91608fc
Francisco Rojo <frojo@andrew.cmu.edu>
2016-02-24 01:58:26 -0500

    Jumping

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+	private PlayerMovement player;
+
+
+	// Use this for initialization
+	void Start () {
+		player = gameObject.GetComponentInParent<PlayerMovement> ();
+	}
+	
+	void OnTriggerEnter2D (Collider2D col) {
+		player.grounded = true;
+	}
+
+	void OnTriggerStay2D (Collider2D col) {
+		player.grounded = true;
+	}
+
+	void OnTriggerExit2D (Collider2D col) {
+		player.grounded = false;
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,30 +3,57 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
-	public float speed = 50f;
+	public float speedForce = 50f;
+	public float maxSpeed = 10f;
 	public float jumpForce = 150f;
 
 	public bool grounded;
 
 	private Rigidbody2D rb2d;
+	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
 
 		rb2d = gameObject.GetComponent<Rigidbody2D> ();
+		anim = gameObject.GetComponent<Animator> ();
 	
 	}
+		
 
 	// Update is called once per frame
 	void Update() {
 
+		anim.SetBool ("Grounded", grounded);
+		anim.SetFloat ("Speed", Mathf.Abs(Input.GetAxis("Horizontal")));
+
+
+		/* Get Player to face correct direction */
+		if (Input.GetAxis ("Horizontal") > 0.1f) {
+			transform.localScale = new Vector3 (1, 1, 1);
+		} else if (Input.GetAxis ("Horizontal") < -0.1f) {
+			transform.localScale = new Vector3 (-1, 1, 1);
+		}
+
+		if (Input.GetButtonDown ("Jump")) {
+			rb2d.AddForce (Vector2.up * jumpForce);
+		}
+
 	}
+
 
 	void FixedUpdate () {
 
+		/* Moving the player */
 		float h = Input.GetAxis ("Horizontal");
+		rb2d.AddForce ((Vector2.right * speedForce) * h);
 
-		rb2d.AddForce ((Vector2.right * speed) * h);
+		/* Limiting speed of Player */
+		if (rb2d.velocity.x > maxSpeed) {
+			rb2d.velocity = new Vector2 (maxSpeed, rb2d.velocity.y);
+		} else if (rb2d.velocity.x < -maxSpeed) {
+			rb2d.velocity = new Vector2 (-maxSpeed, rb2d.velocity.y);
+		}
 
 
 	
