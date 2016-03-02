ba3e10d62c403be3636ab9e3991aee18f5688b0a
Francisco Rojo <frojo@andrew.cmu.edu>
2016-03-01 20:48:48 -0500

    some work on guard

---
+++ Assets/Scripts/FireChoco.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireChoco : MonoBehaviour {
+
+	public GameObject explosion;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// when it hits anything
+	void OnCollisionEnter2D(Collision2D c) {
+
+		if (c.gameObject.tag == "Baddie")
+			return;
+
+		// hurt other thing (look at gun2d code)
+		// destroy self
+
+		/* Explode on contact with anything */
+		if (explosion)
+			Object.Instantiate (explosion, transform.position, Quaternion.identity);
+		Destroy (gameObject);
+	}
+}
